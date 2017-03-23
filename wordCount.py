aa35eadf618bbc5a8059dff90dc49d2aa26c4dc8
Sudeshna Pal <sudeshnapal12@gmail.com>
2017-03-23 00:25:02 -0400

    Update wordCount.py

--- wordCount.py
+++ wordCount.py
@@ -8,7 +8,7 @@ from operator import add
 
 # Creating Spark Context
 sc = SparkContext(appName="Word Count")
-text_file = sc.textFile("README.md")
+text_file = sc.textFile("SampleFileWordCount.txt")
 counts = text_file.flatMap(lambda line: line.split(" ")) \
              .map(lambda word: (word, 1)) \
              .reduceByKey(lambda a, b: a + b)
